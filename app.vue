37559f30c3bcd8b22c8139707cb5ec0850215e95
Jose Sanchez <sanchezpepe97@gmail.com>
2023-05-28 00:19:36 -0600

    added pinia

--- app.vue
+++ app.vue
@@ -2,6 +2,32 @@
   <h1 class="text-3xl font-bold underline">Hello world!</h1>
 
   <div>
+    <p>Count: {{ counter.count }}</p>
+    <button @click="counter.increment()">Increment</button>
+    <button @click="counter.decrement()">Decrement</button>
+  </div>
+
+  <div>
     <NuxtWelcome />
   </div>
 </template>
+
+<script>
+import { useStore } from "~/store/mainStore.js";
+import { reactive, toRefs } from "vue";
+
+export default {
+  setup() {
+    const state = reactive({
+      count: 0,
+    });
+
+    const counter = useStore();
+
+    return {
+      counter,
+      ...toRefs(state),
+    };
+  },
+};
+</script>
